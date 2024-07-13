575a80c19b9cadf3b162f55e2471bcf69729ec32
Sonnandh Kalaka <sonnandh@amazon.com>
2024-07-13 22:20:39 +0530

    Add run script for x86

---
+++ scripts/run_x86.sh
@@ -0,0 +1,7 @@
+lscpu
+perf stat ./basic_spsc_queue
+perf stat ./basic_spsc_without_modulo_queue
+perf stat ./spsc_ra_pairs
+perf stat ./spsc_without_fs
+perf stat ./spsc_local_cache
+perf ./rigtorp_spsc
