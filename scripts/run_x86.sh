fc4ca0b596dd789c8c9832963138b332e6b015ba
Sonnandh Kalaka <sonnandh@amazon.com>
2024-07-14 23:35:04 +0530

    Update scripts for x86

--- scripts/run_x86.sh
+++ scripts/run_x86.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+# Print CPU information
 lscpu
-perf stat ./basic_spsc_queue
-perf stat ./basic_spsc_without_modulo_queue
-perf stat ./spsc_ra_pairs
-perf stat ./spsc_without_fs
-perf stat ./spsc_local_cache
-perf ./rigtorp_spsc
+
+# Run performance stats on each executable in the ../release/ folder
+perf stat ../release/basic_spsc_queue
+perf stat ../release/basic_spsc_without_modulo_queue
+perf stat ../release/spsc_ra_pairs
+perf stat ../release/spsc_without_fs
+perf stat ../release/spsc_local_cache
+perf stat ../release/rigtorp_spsc
